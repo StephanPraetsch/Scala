20a3574bd6b5ef6f612e6de468e79646dad3dc69
Stephan Praetsch <Stephan.Praetsch@mercateo.com>
2018-09-17 09:46:05 +0200

    updated to toString

--- src/basics/_5_objects/Person.scala
+++ src/basics/_5_objects/Person.scala
@@ -1,13 +1,13 @@
 package basics._5_objects
 
-// case class: each param becomes a member
 case class Person(firstName: String, lastName: String, age: Int) {
   def isAdult: Boolean = if (age >= 18) true else false
+  override def toString() = firstName + " " + lastName + " is " + (if (isAdult) "" else "not ") + "adult"
 }
 
 object MainPerson extends App {
 
   val p = new Person("foo", "bar", 20);
-  println(p + " isAdult=" + p.isAdult)
+  println(p)
 
 }
