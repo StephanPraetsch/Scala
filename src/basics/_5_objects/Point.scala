ae9acd98e31ebfb86c6149862f46e58b2d082051
Stephan Praetsch <Stephan.Praetsch@mercateo.com>
2018-10-19 13:26:53 +0200

    added subclass Location as example class

--- src/basics/_5_objects/Point.scala
+++ src/basics/_5_objects/Point.scala
@@ -10,9 +10,25 @@ class Point(val xc: Int, val yc: Int) {
   def move(dx: Int, dy: Int) {
     x = x + dx
     y = y + dy
-    println("Point x location : " + x);
-    println("Point y location : " + y);
   }
+
+  override def toString() = "Point[" + x + "," + y + "]"
+
+}
+
+class Location(override val xc: Int, override val yc: Int, val zc: Int) extends Point(xc, yc) {
+
+  var z: Int = zc
+
+  def move(dx: Int, dy: Int, dz: Int) {
+    x = x + dx
+    y = y + dy
+    z = z + dz
+  }
+
+  // TODO dont know how override toString here
+  //  override def toString() = "Location[" + x + "," + y + "," + z + "]"
+
 }
 
 object Demo {
@@ -20,6 +36,12 @@ object Demo {
   def main(args: Array[String]) {
     val pt = new Point(10, 20);
     pt.move(10, 10);
+    println(pt)
+
+    val loc = new Location(10, 20, 30)
+    loc.move(1, 2, 10);
+    println(loc)
+
   }
 
 }
